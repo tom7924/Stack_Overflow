6716fcf61a88afd926b575383f4560f97ec6c416
KIWOONG KIM <tom777416@gmail.com>
2025-06-26 22:42:55 +0900

    make character move using Input Manager
    make character move using Input Manager

--- Assets/Warrior free set/code/Player.cs
+++ Assets/Warrior free set/code/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
@@ -13,12 +14,6 @@ public class Player : MonoBehaviour
         rigid = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
-    {
-        inputVec.x = Input.GetAxisRaw("Horizontal");
-        inputVec.y = Input.GetAxisRaw("Vertical");
-    }
-
     private void FixedUpdate()
     {
         // 1. 힘을 준다
@@ -26,8 +21,13 @@ public class Player : MonoBehaviour
         // 2. 속도 제어
         //rigid.linearVelocity = inputVec;
         // 3. 위치 이동
-        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
         rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
     }
     
 }
