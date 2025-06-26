f102f408e56fe86e45ae111e24186e241fe5504b
KIWOONG KIM <tom777416@gmail.com>
2025-06-27 02:34:25 +0900

    add running, standing, dead animation
    add running, standing, dead animation

--- Assets/Warrior free set/code/Player.cs
+++ Assets/Warrior free set/code/Player.cs
@@ -8,10 +8,14 @@ public class Player : MonoBehaviour
     public Vector2 inputVec;
     public float speed;
     Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
@@ -28,6 +32,16 @@ public class Player : MonoBehaviour
     void OnMove(InputValue value)
     {
         inputVec = value.Get<Vector2>();
+    }
+
+    private void LateUpdate() {
+
+        anim.SetFloat("Speed",inputVec.magnitude);
+
+        if (inputVec.x != 0)
+        {
+            spriter.flipX = inputVec.x < 0;
+        }
     }
     
 }
