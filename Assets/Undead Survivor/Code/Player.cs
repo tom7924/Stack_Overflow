2534b341cddc8d7d698ea0de129727e90dd54db4
KIWOONG KIM <tom777416@gmail.com>
2025-06-28 17:17:31 +0900

    add Character Dash
    Character Dash when player press spacebar

--- Assets/Undead Survivor/Code/Player.cs
+++ Assets/Undead Survivor/Code/Player.cs
@@ -6,11 +6,26 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [Header("Movement")]
     public Vector2 inputVec;
     public float speed;
+
+    [Header("Dash")]
+    public float dashSpeed = 20f;
+    public float dashDuration = 0.2f;
+    public float dashCooldownDuration = 1f;
+
+
     Rigidbody2D rigid;
     SpriteRenderer spriter;
     Animator anim;
+
+    private bool isDashing = false;
+    private Vector2 dashDirection;
+    private float dashRemainingTime;
+    private float dashCooldownTimer;
+    private Vector2 lastMoveDirection = Vector2.down;
+    
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
@@ -20,9 +35,78 @@ public class Player : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
-        rigid.MovePosition(rigid.position + nextVec);
-        
+        UpdateTimers();
+
+        if (isDashing)
+        {
+            Vector2 dashVec = dashDirection * dashSpeed * Time.fixedDeltaTime;
+            rigid.MovePosition(rigid.position + dashVec);
+
+            if (dashRemainingTime <= 0)
+            {
+                EndDash();
+            }
+        }
+        else
+        {
+            Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
+            rigid.MovePosition(rigid.position + nextVec);
+
+            if (inputVec != Vector2.zero)
+            {
+                lastMoveDirection = inputVec.normalized;
+            }
+        }
+    }
+
+    void UpdateTimers()
+    {
+        if (isDashing)
+        {
+            dashRemainingTime -= Time.fixedDeltaTime;
+        }
+
+        if (dashCooldownTimer > 0)
+        {
+            dashCooldownTimer -= Time.fixedDeltaTime;
+        }
+    }
+
+    void OnDash(InputValue value)
+    {
+        Debug.Log("OnDash 호출됨! isPressed: " + value.isPressed);
+        if (value.isPressed && CanDash())
+        {
+            Debug.Log("대시 시작!");
+            StartDash();   
+        }
+            
+    }
+
+    bool CanDash()
+    {
+        return !isDashing && dashCooldownTimer <= 0;
+    }
+
+    void StartDash()
+    {
+        isDashing = true;
+        dashRemainingTime = dashDuration;
+
+        if (inputVec != Vector2.zero)
+        {
+            dashDirection = inputVec.normalized;
+        }
+        else
+        {
+            dashDirection = lastMoveDirection;
+        }
+    }
+
+    void EndDash()
+    {
+        isDashing = false;
+        dashCooldownTimer = dashCooldownDuration;
     }
 
     void OnMove(InputValue value)
