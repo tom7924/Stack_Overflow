7ddedccebefb5070468a63b2cccf014e73b7cd82
KIWOONG KIM <tom777416@gmail.com>
2025-07-01 01:31:41 +0900

    rotating weapon's center is changed into weaponpivot
    rotating weapon's center is changed into weaponpivot and little bit of modify at attack action

--- Assets/Undead Survivor/Code/Hands.cs
+++ Assets/Undead Survivor/Code/Hands.cs
@@ -2,27 +2,39 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using JetBrains.Annotations;
 
 public class Hands : MonoBehaviour
 {
+    public Transform weaponPivot;
     public bool isLeft;
     public SpriteRenderer spriter;
     public SpriteRenderer bodyRenderer;
     SpriteRenderer player;
 
-    Vector3 rightPos = new Vector3(0.037f, -0.013f, 0);
+    Vector3 rightPos = new Vector3(0.127f, -0.116f, 0);
     Vector3 rightPosReverse = new Vector3(-0.153f, 0.117f, 0);
 
     Vector3 leftPos = new Vector3(-0.15f, -0.25f, 0);
     Vector3 leftPosReverse = new Vector3(-0.25f, -0.25f, 0);
 
+    Vector3 pivotPos = new Vector3(0.192f, -0.2f, 0);
+    Vector3 pivotPosReverse = new Vector3(0.13f, -0.25f, 0);
+
     private Vector3 OriginalRotation;
+    private Vector3 OriginalPivotRotation;
+
 
 
     void Awake()
     {
         player = bodyRenderer;
         OriginalRotation = transform.eulerAngles;
+        if (weaponPivot == null)
+        {
+            weaponPivot = transform.parent;
+        }
+        OriginalPivotRotation = weaponPivot.eulerAngles;
     }
 
     void LateUpdate()
@@ -36,6 +48,7 @@ public class Hands : MonoBehaviour
         else
         {
             transform.localPosition = isReverse ? rightPosReverse : rightPos;
+            weaponPivot.localPosition = isReverse ? pivotPosReverse : pivotPos;
             spriter.flipY = isReverse;
             spriter.sortingOrder = isReverse ? 3 : 1;
         }
@@ -48,15 +61,15 @@ public class Hands : MonoBehaviour
         {
             if (isReverse)
             {
-                transform.DORotate(new Vector3(0, 0, 0), 0.15f, RotateMode.FastBeyond360)
+                weaponPivot.DORotate(new Vector3(0, 0, 0), 0.20f, RotateMode.FastBeyond360)
                     .SetEase(Ease.OutQuad)
                     .OnComplete(() =>
                     {
-                        transform.DORotate(new Vector3(0, 0, 210), 0.15f, RotateMode.FastBeyond360)
+                        weaponPivot.DORotate(new Vector3(0, 0, 240), 0.20f, RotateMode.FastBeyond360)
                             .SetEase(Ease.OutQuad)
                             .OnComplete(() =>
                             {
-                                transform.DORotate(OriginalRotation, 0.15f)
+                                weaponPivot.DORotate(OriginalPivotRotation, 0.20f, RotateMode.FastBeyond360)
                                     .SetEase(Ease.InQuad)
                                     .OnComplete(() =>
                                     {
@@ -67,15 +80,15 @@ public class Hands : MonoBehaviour
             }
             else
             {
-                transform.DORotate(new Vector3(0, 0, 90), 0.15f , RotateMode.FastBeyond360)
+                weaponPivot.DORotate(new Vector3(0, 0, 90), 0.15f, RotateMode.FastBeyond360)
                     .SetEase(Ease.OutQuad)
                     .OnComplete(() =>
                     {
-                        transform.DORotate(new Vector3(0, 0, -120), 0.15f, RotateMode.FastBeyond360)
+                        weaponPivot.DORotate(new Vector3(0, 0, -120), 0.15f, RotateMode.FastBeyond360)
                             .SetEase(Ease.OutQuad)
                             .OnComplete(() =>
                             {
-                                transform.DORotate(OriginalRotation, 0.15f)
+                                weaponPivot.DORotate(OriginalPivotRotation, 0.15f)
                                     .SetEase(Ease.InQuad)
                                     .OnComplete(() =>
                                     {
